f5b4ee0a750e71fc7e2c1affbc428bd4c7279bd6
einToast <laurin2102@gmail.com>
2022-10-18 16:01:44 +0200

    Initial commit

---
+++ src/main/java/de/thb/anylearn/AnyLearnApplication.java
@@ -0,0 +1,13 @@
+package de.thb.anylearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/thb/anylearn/AnyLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thb.anylearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
